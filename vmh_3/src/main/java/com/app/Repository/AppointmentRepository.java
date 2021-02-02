314883c3897bb48a72cb75bace031cc3df29e2f5
Shrutika Deore <75741271+Shrutika7697@users.noreply.github.com>
2021-02-02 15:58:01 +0530

    Delete vmh_3 directory

--- vmh_3/src/main/java/com/app/Repository/AppointmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.pojos.Appointment;
-
-public interface AppointmentRepository extends  JpaRepository< Appointment ,Integer> {
-
-}
